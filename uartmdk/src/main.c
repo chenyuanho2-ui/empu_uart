4a5947853f8030d60fd05c15b77ff9bfd17d5a18
chenyuanho2-ui <chenyuanho2@gmail.com>
2026-03-13 15:42:42 +0800

    增加打印

--- uartmdk/src/main.c
+++ uartmdk/src/main.c
@@ -2,36 +2,41 @@
 #include "gw1ns4c_uart.h"    
 #include "gw1ns4c_syscon.h"  
 
+// 把函数名改成 My_UART_SendString，避免和官方库同名冲突
+void My_UART_SendString(UART_TypeDef* UARTx, char *str) {
+    while (*str != '\0') {
+        while (UARTx->STATE & 0x01); // 等待发送缓冲空
+        UART_SendChar(UARTx, *str);  // 发送单个字符
+        str++;
+    }
+}
+
 int main(void) {
     UART_InitTypeDef UART_InitStruct;
 
-    // 1. 系统初始化
-    SystemInit();
+    SystemInit(); 
 
-    // 2. 初始化 UART0
-    UART_InitStruct.UART_Mode.UARTMode_Tx = ENABLE;     // 开启发送
-    UART_InitStruct.UART_Mode.UARTMode_Rx = ENABLE;     // 开启接收
-    UART_InitStruct.UART_Int.UARTInt_Tx = DISABLE;      // 轮询模式，关闭发送中断
-    UART_InitStruct.UART_Int.UARTInt_Rx = DISABLE;      // 轮询模式，关闭接收中断
-    UART_InitStruct.UART_BaudRate = 115200;             // 设置与电脑串口助手一致的波特率
+    UART_InitStruct.UART_Mode.UARTMode_Tx = ENABLE;     
+    UART_InitStruct.UART_Mode.UARTMode_Rx = ENABLE;     
+    UART_InitStruct.UART_Int.UARTInt_Tx = DISABLE;      
+    UART_InitStruct.UART_Int.UARTInt_Rx = DISABLE;      
+    UART_InitStruct.UART_BaudRate = 115200;             
 
     UART_Init(UART0, &UART_InitStruct);
 
-    // 3. 进入主循环：轮询回环
     while(1) {
-        // 检查 STATE 寄存器的第 1 位 (0x02)
-        // 如果为真，说明 RX 缓冲区收到数据
-        if (UART0->STATE & 0x02) {
+        if (UART0->STATE & 0x02) { // 接收缓冲有数据
             
-            // 使用 4C 专用的接收函数读取数据
             uint8_t rx_data = (uint8_t)UART_ReceiveChar(UART0);
-
-            // 检查 STATE 寄存器的第 0 位 (0x01)
-            // 如果为 1，说明 TX 缓冲区正满，死等它把之前的数据发完
+            
+            // 调用我们自己改名后的发送字符串函数
+            My_UART_SendString(UART0, "char is ");
+            
             while (UART0->STATE & 0x01);
-
-            // 使用 4C 专用的发送函数把刚才读到的数据原样发回给电脑
             UART_SendChar(UART0, rx_data);
+            
+            // 发送回车换行
+            My_UART_SendString(UART0, "\r\n");
         }
     }
 }
